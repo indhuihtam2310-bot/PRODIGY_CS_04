f133eec0a70f1355df04033302beb4d571171a00
IndhuMathi <indhuihtam2310@gmail.com>
2025-11-11 13:25:05 +0530

    Fix comment formatting in Simple_keylogger.py
    Corrected comment formatting and spacing in the keylogger file.

--- Simple_keylogger.py
+++ Simple_keylogger.py
@@ -1,5 +1,5 @@
 # PRODIGY_CS_04
-#I have Created keylogger program that records and logs the keystokes., that will focus on logging the keys pressed and saving them to a file.
+# I have Created keylogger program that records and logs the keystokes., that will focus on logging the keys pressed and saving them to a file.
 
 import pynput
 import time
